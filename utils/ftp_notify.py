17d315dacaf3ce205ac12796d5ecdb43ab4b7c50
derPuntigamer <daniel.dudola@gmail.com>
2017-12-13 00:05:11 +0100

    adds FTP notifier

---
+++ utils/ftp_notify.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+import ftputil
+import os
+from datetime import datetime
+
+dir_name = os.path.dirname(os.path.realpath(__file__))
+lastversion_path = dir_name + "/lastversion.txt"
+
+with ftputil.FTPHost("ftp.ebi.ac.uk", "anonymous", "anonymous") as ftp_host:
+    ftp_path_trembl = "pub/databases/uniprot/knowledgebase/uniprot_trembl.fasta.gz"
+    ftp_path_sprot = "pub/databases/uniprot/knowledgebase/uniprot_sprot.fasta.gz"
+    current_timestamp = ftp_host.path.getmtime(ftp_path_trembl)
+
+    with open(lastversion_path) as fp:  
+        saved_timestamp = float(fp.readline())
+        print(saved_timestamp)
+        print(current_timestamp)
+        if saved_timestamp == current_timestamp:
+            print("No new upload found.")
+            raise SystemExit(0)
+
+    print("New upload found.")
+
+    with open(lastversion_path, "w") as lastversion_file:
+        lastversion_file.write(str(current_timestamp))
+
+    ftp_host.download(ftp_path_trembl, str(current_timestamp) + "_uniprot_trembl.fasta.gz")
+    ftp_host.download(ftp_path_sprot, str(current_timestamp) + "_uniprot_sprot.fasta.gz")
+
+
+sent_from = "bioinf.notification@gmail.com@gmail.com"
+to = ["daniel.dudola@gmail.com"]  
+subject = "OMG Super Important Message"
+body = "Subject: CSAH server notification\n\nNew database is available!"
+
+gmail_user = "bioinf.notification@gmail.com"
+gmail_password = ""
+
+
+import smtplib
+
+try:  
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(gmail_user, gmail_password)
+    server.sendmail(sent_from, to, body)
+    server.close()
+    print("Notification e-mail sent.")
+
+except Exception as e:  
+    print("[ERROR] e-mail was not sent", e)
