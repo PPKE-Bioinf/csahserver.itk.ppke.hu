71ec7fa02d4d4eb2aa3f45cbd9aca96290b31cd8
derPuntigamer <daniel.dudola@gmail.com>
2018-01-18 13:39:09 +0100

    add: configurable download path

--- utils/ftp_notify.py
+++ utils/ftp_notify.py
@@ -6,12 +6,17 @@ from datetime import datetime
 
 import yaml
 
+DATE = datetime.now()
 dir_name = os.path.dirname(os.path.realpath(__file__))
 
 with open(dir_name + "/config.yml", 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
 
+SAVE_PATH = cfg["downloadPath"] + str(DATE.year) + "_" + str(DATE.month)
 lastversion_path = dir_name + "/lastversion.txt"
+
+if not os.path.isdir(SAVE_PATH):
+    os.makedirs(SAVE_PATH)
 
 with ftputil.FTPHost("ftp.ebi.ac.uk", "anonymous", "anonymous") as ftp_host:
     ftp_path_trembl = "pub/databases/uniprot/knowledgebase/uniprot_trembl.fasta.gz"
@@ -19,7 +24,7 @@ with ftputil.FTPHost("ftp.ebi.ac.uk", "anonymous", "anonymous") as ftp_host:
     current_timestamp = ftp_host.path.getmtime(ftp_path_trembl)
 
     try:
-        with open(lastversion_path) as fp:  
+        with open(lastversion_path) as fp:
             saved_timestamp = float(fp.readline())
             print(saved_timestamp)
             print(current_timestamp)
@@ -34,12 +39,12 @@ with ftputil.FTPHost("ftp.ebi.ac.uk", "anonymous", "anonymous") as ftp_host:
     with open(lastversion_path, "w") as lastversion_file:
         lastversion_file.write(str(current_timestamp))
 
-    ftp_host.download(ftp_path_trembl, str(current_timestamp) + "_uniprot_trembl.fasta.gz")
-    ftp_host.download(ftp_path_sprot, str(current_timestamp) + "_uniprot_sprot.fasta.gz")
+    ftp_host.download(ftp_path_trembl, SAVE_PATH + "/uniprot_trembl.fasta.gz")
+    ftp_host.download(ftp_path_sprot, SAVE_PATH + "/uniprot_sprot.fasta.gz")
 
 
 sent_from = "bioinf.notification@gmail.com@gmail.com"
-to = cfg['addresses'] 
+to = cfg['addresses']
 subject = "OMG Super Important Message"
 body = "Subject: CSAH server notification\n\nNew database is available!"
 
@@ -49,7 +54,7 @@ gmail_password = cfg['password']
 
 import smtplib
 
-try:  
+try:
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.ehlo()
     server.starttls()
@@ -59,5 +64,5 @@ try:
     server.close()
     print("Notification e-mail sent.")
 
-except Exception as e:  
+except Exception as e:
     print("[ERROR] e-mail was not sent", e)
